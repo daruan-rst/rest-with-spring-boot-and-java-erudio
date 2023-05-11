2043d03884e41e88e6d562d72f84a72c2c6272e5
daruan-rst <daruan.rst@gmail.com>
2023-05-11 13:21:09 -0300

    0803 Implementando PersonEntity e o Repositório com Hibernate e Spring Data JPA

--- 08_ConnectingToMySQL/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/eurdio/model/Person.java
+++ 08_ConnectingToMySQL/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/eurdio/model/Person.java
@@ -1,17 +1,30 @@
 package br.com.eurdio.model;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "person")
 public class Person implements Serializable {
 
     private static final Long serialVersionUID =  1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "first_name", nullable = false, length = 80)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 80)
     private String lastName;
+
+    @Column(nullable = false, length = 80)
     private String address;
+
+    @Column(nullable = false, length = 6)
     private String gender;
     public Person() {
     }
