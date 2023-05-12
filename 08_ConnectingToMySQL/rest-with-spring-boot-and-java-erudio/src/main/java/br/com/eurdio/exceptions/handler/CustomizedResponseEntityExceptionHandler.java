3f782afaa8e84d9301309621394082ea230a164a
daruan-rst <daruan.rst@gmail.com>
2023-05-12 00:21:24 -0300

    0804 Substituindo os Mocks do Service pelo Acesso ao Repositório

--- 08_ConnectingToMySQL/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/eurdio/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ 08_ConnectingToMySQL/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/eurdio/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -1,7 +1,7 @@
 package br.com.eurdio.exceptions.handler;
 
 import br.com.eurdio.exceptions.ExceptionResponse;
-import br.com.eurdio.exceptions.UnsupportedMathOperationException;
+import br.com.eurdio.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -28,15 +28,15 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 
     }
 
-    @ExceptionHandler(UnsupportedMathOperationException.class)
-    public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(
             Exception ex, WebRequest request){
 
         ExceptionResponse exceptionResponse = new ExceptionResponse(
                 new Date(),
                 ex.getMessage(),
                 request.getDescription(false));
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
         
     }
 
--- 08_ConnectingToMySQL/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/eurdio/services/PersonServices.java
+++ 08_ConnectingToMySQL/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/eurdio/services/PersonServices.java
@@ -1,68 +1,66 @@
 package br.com.eurdio.services;
 
+import br.com.eurdio.exceptions.ResourceNotFoundException;
 import br.com.eurdio.model.Person;
+import br.com.eurdio.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
 @Service
 public class PersonServices {
 
-    private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    @Autowired
+    PersonRepository repository;
 
     public List<Person> findAll(){
         logger.info("Finding one person!");
-        List<Person> persons = new ArrayList<>();
-        for (int i = 0; i < 8; i++) {
-            Person person = mockPerson(i);
-            persons.add(person);
-        }
-        return persons;
+
+        return repository.findAll();
     }
 
-    public Person findById(String id){
+    public Person findById(Long id){
         logger.info("Finding one person!");
 
-        Person person = new Person();
-        person.setId(counter.incrementAndGet());
-        person.setFirstName("Leandro");
-        person.setLastName("Costa");
-        person.setAddress("Uberlândia - Minas Gerais - Brasil");
-        person.setGender("Male");
 
-        return person;
+
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
     }
 
     public Person create(Person person){
+
         logger.info("Creating one person!");
 
-        return person;
+        return repository.save(person);
     }
 
     public Person update(Person person){
+
         logger.info("Updating one person!");
 
-        return person;
+        Person entity = repository.findById(person.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+        entity.setFirstName(person.getFirstName());
+        entity.setLastName(person.getLastName());
+        entity.setAddress(person.getAddress());
+        entity.setGender(person.getGender());
+
+        return repository.save(entity);
     }
 
-    public void delete(String id){
+    public void delete(Long id){
         logger.info("Deleting one person!");
 
+        Person entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+        repository.delete(entity);
     }
 
-    private Person mockPerson(int i) {
-        Person person = new Person();
-        person.setId(counter.incrementAndGet());
-        person.setFirstName("Person name " + i);
-        person.setLastName("Last name " + i);
-        person.setAddress("Some Address in Brazil " +i);
-        person.setGender("Male");
-
-        return person;
-    }
 
 }
