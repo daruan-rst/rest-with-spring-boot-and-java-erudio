8d47bb6f7da5d8b34a695bea9c5a42097f1995be
daruan-rst <daruan.rst@gmail.com>
2023-05-14 21:54:34 -0300

    0906 [UPDATES] Diferenças entre o Spring Boot 3 Milestone e Spring Boot 3.0.1

--- 09_WorkingWithVOPattern/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/eurdio/mapper/DozerMapper.java
+++ 09_WorkingWithVOPattern/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/eurdio/mapper/DozerMapper.java
@@ -1,20 +1,17 @@
 package br.com.eurdio.mapper;
 
-//import br.com.eurdio.data.vO.v1.PersonVO;
-//import com.github.dozermapper.core.DozerBeanMapperBuilder;
-//import com.github.dozermapper.core.Mapper;
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
 
-
-
-import org.modelmapper.ModelMapper;
+//import org.modelmapper.ModelMapper;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Mapper {
+public class DozerMapper {
 
-//    private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
-    private static ModelMapper mapper = new ModelMapper();
+    private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+//    private static ModelMapper mapper = new ModelMapper();
 
     public static <O, D> D parseObject(O origin, Class<D> destination){
         return mapper.map(origin, destination);
