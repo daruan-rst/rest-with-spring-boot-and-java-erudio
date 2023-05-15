acffa5d129bbcb290216d550153863afe278e726
daruan-rst <daruan.rst@gmail.com>
2023-05-14 21:18:17 -0300

    0903 Implementando o Padrão de Projetos VO

---
+++ 09_WorkingWithVOPattern/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/eurdio/services/PersonServices.java
@@ -0,0 +1,66 @@
+package br.com.eurdio.services;
+
+import br.com.eurdio.data.vO.v1.PersonVO;
+import br.com.eurdio.exceptions.ResourceNotFoundException;
+import br.com.eurdio.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@Service
+public class PersonServices {
+
+    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    @Autowired
+    PersonRepository repository;
+
+    public List<PersonVO> findAll(){
+        logger.info("Finding one person!");
+
+        return repository.findAll();
+    }
+
+    public PersonVO findById(Long id){
+        logger.info("Finding one person!");
+
+
+
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+    }
+
+    public PersonVO create(PersonVO person){
+
+        logger.info("Creating one person!");
+
+        return repository.save(person);
+    }
+
+    public PersonVO update(PersonVO person){
+
+        logger.info("Updating one person!");
+
+        PersonVO entity = repository.findById(person.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+        entity.setFirstName(person.getFirstName());
+        entity.setLastName(person.getLastName());
+        entity.setAddress(person.getAddress());
+        entity.setGender(person.getGender());
+
+        return repository.save(entity);
+    }
+
+    public void delete(Long id){
+        logger.info("Deleting one person!");
+
+        PersonVO entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+        repository.delete(entity);
+    }
+
+
+}
