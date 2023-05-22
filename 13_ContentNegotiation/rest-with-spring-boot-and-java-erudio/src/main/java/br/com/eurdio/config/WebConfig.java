01facca5c0091a2ef630ad123647e95a322e0369
daruan-rst <daruan.rst@gmail.com>
2023-05-21 23:53:26 -0300

    1304 Content Negotiation via HEADER Parameter

--- 13_ContentNegotiation/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/eurdio/config/WebConfig.java
+++ 13_ContentNegotiation/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/eurdio/config/WebConfig.java
@@ -12,12 +12,22 @@ public class WebConfig implements WebMvcConfigurer {
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         // Via EXTENSION. http://localhost:8080/api/person/v1.xml DEPRECATED on SpringBoot 2.6
 
-        // Via QUERY PARAM. http://localhost:8080/api/person/v1?mediaType=xml
+		// Via QUERY PARAM. http://localhost:8080/api/person/v1?mediaType=xml
 
+		/*
 		configurer.favorParameter(true)
-			.parameterName("mediaType").ignoreAcceptHeader(true)
-			.useRegisteredExtensionsOnly(false)
-			.defaultContentType(MediaType.APPLICATION_JSON)
+				.parameterName("mediaType").ignoreAcceptHeader(true)
+				.useRegisteredExtensionsOnly(false)
+				.defaultContentType(MediaType.APPLICATION_JSON)
+				.mediaType("json", MediaType.APPLICATION_JSON)
+				.mediaType("xml", MediaType.APPLICATION_XML);
+		*/
+		// Via HEADER PARAM. http://localhost:8080/api/person/v1
+
+		configurer.favorParameter(false)
+				.ignoreAcceptHeader(false)
+				.useRegisteredExtensionsOnly(false)
+				.defaultContentType(MediaType.APPLICATION_JSON)
 				.mediaType("json", MediaType.APPLICATION_JSON)
 				.mediaType("xml", MediaType.APPLICATION_XML);
 
