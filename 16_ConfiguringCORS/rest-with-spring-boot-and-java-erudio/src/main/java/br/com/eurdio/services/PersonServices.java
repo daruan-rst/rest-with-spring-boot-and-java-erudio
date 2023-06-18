83129c1d40c63ad6570f04371a77026ca9d61bf5
daruan-rst <daruan.rst@gmail.com>
2023-06-17 21:21:49 -0300

    1603 Como Habilitar CORS no Spring Framework (Spring Boot)

---
+++ 16_ConfiguringCORS/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/eurdio/services/PersonServices.java
@@ -0,0 +1,95 @@
+package br.com.eurdio.services;
+
+import br.com.eurdio.controller.PersonController;
+import br.com.eurdio.data.vO.v1.PersonVO;
+import br.com.eurdio.exceptions.RequiredObjectIsNullException;
+import br.com.eurdio.exceptions.ResourceNotFoundException;
+import br.com.eurdio.mapper.DozerMapper;
+import br.com.eurdio.model.Person;
+import br.com.eurdio.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import java.util.List;
+import java.util.logging.Logger;
+
+@Service
+public class PersonServices {
+
+    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    @Autowired
+    PersonRepository repository;
+
+    public List<PersonVO> findAll() {
+        logger.info("Finding all people!");
+
+        var persons =  DozerMapper.parseListObjects(repository.findAll(), PersonVO.class);
+        persons
+            .stream()
+            .forEach(p -> {
+                try {
+                    p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel());
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        return persons;
+    }
+
+    public PersonVO findById(Long id) throws Exception {
+        logger.info("Finding one person!");
+
+        var entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+        var vo = DozerMapper.parseObject(entity, PersonVO.class);
+        vo.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
+        return vo;
+    }
+
+    public PersonVO create(PersonVO person) throws Exception {
+
+        if (person == null){
+            throw new RequiredObjectIsNullException();
+        }
+        logger.info("Creating one person!");
+        var entity = DozerMapper.parseObject(person, Person.class);
+        var vo = DozerMapper.parseObject(repository.save(entity), PersonVO.class);
+        vo.add(linkTo(methodOn(PersonController.class).findById(vo.getKey())).withSelfRel());
+        return vo;
+    }
+
+    public PersonVO update(PersonVO person) throws Exception {
+
+        if (person == null){
+            throw new RequiredObjectIsNullException();
+        }
+
+        logger.info("Updating one person!");
+
+        Person entity = repository.findById(person.getKey())
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+        entity.setFirstName(person.getFirstName());
+        entity.setLastName(person.getLastName());
+        entity.setAddress(person.getAddress());
+        entity.setGender(person.getGender());
+
+        var vo = DozerMapper.parseObject(repository.save(entity), PersonVO.class);
+        vo.add(linkTo(methodOn(PersonController.class).findById(vo.getKey())).withSelfRel());
+        return vo;
+    }
+
+    public void delete(Long id){
+        logger.info("Deleting one person!");
+
+        var entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+        repository.delete(entity);
+    }
+
+
+}
