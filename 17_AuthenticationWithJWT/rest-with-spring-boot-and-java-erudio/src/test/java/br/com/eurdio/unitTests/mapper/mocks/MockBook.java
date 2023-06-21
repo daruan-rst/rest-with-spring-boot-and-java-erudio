c51bcfefdc1527d3f4685d42e6417adfef40e131
daruan-rst <daruan.rst@gmail.com>
2023-06-21 12:55:15 -0300

    1701 Apresentação da Seção - Autenticação com JWT e Spring Security

---
+++ 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/eurdio/unitTests/mapper/mocks/MockBook.java
@@ -0,0 +1,31 @@
+package br.com.eurdio.unitTests.mapper.mocks;
+
+import br.com.eurdio.model.Book;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class MockBook {
+
+    public List<Book> mockBookList(){
+        Function<Long, Book> mockABook = id -> mockBookEntity(id);
+        List<Long> listOfIds = List.of(1L,2L,3L,4L,5L);
+        List<Book> bookList = listOfIds.stream().map(mockABook).collect(Collectors.toList());
+        return bookList;
+    }
+
+    public Book mockBookEntity(Long id){
+        Book book = new Book();
+        book.setId(id);
+        book.setAuthor("Ernest Hemingway");
+        book.setTitle("For Whom The Bell Toes");
+        book.setPrice(new BigDecimal(id*10));
+        book.setLaunchDate(Date.valueOf(LocalDateTime.of(1940, 10, 21,12,0).toLocalDate()));
+        return book;
+    }
+}
