d354033a5cadf5ccb96b7ab30521d5d4ea746892
daruan-rst <daruan.rst@gmail.com>
2023-06-29 22:25:02 -0300

    1710 Implementando o SecurityConfig

--- 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/eurdio/config/SecurityConfig.java
+++ 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/eurdio/config/SecurityConfig.java
@@ -6,58 +6,64 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.HashMap;
 import java.util.Map;
 
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+@EnableWebSecurity
+public class SecurityConfig {
 
     @Autowired
     private JwtTokenProvider tokenProvider;
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    PasswordEncoder passwordEncoder() {
         Map<String, PasswordEncoder> encoders = new HashMap<>();
-        encoders.put("pbkdf2", new Pbkdf2PasswordEncoder());
+        Pbkdf2PasswordEncoder pbkdf2Encoder = new Pbkdf2PasswordEncoder("", 8, 185000, Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+        encoders.put("pbkdf2", pbkdf2Encoder);
         DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
-        passwordEncoder.setDefaultPasswordEncoderForMatches(new Pbkdf2PasswordEncoder());
+        passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
         return passwordEncoder;
     }
 
     @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+    AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return         http
                 .httpBasic().disable()
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                        authorizeHttpRequests -> authorizeHttpRequests
+                                                .requestMatchers(
+                                            "/auth/signin",
+                                                        "/auth/refresh/**",
+                                                        "/swagger-ui.html**",
+                                                        "/v3/api-docs/")
+                                                        .permitAll()
+                                                .requestMatchers("/api/v1/**").authenticated()
+                                                .requestMatchers("/users").denyAll()
+                )
+                .cors()
                 .and()
-                .authorizeRequests()
-                .antMatchers(
-                        "/auth/signin",
-                        "/auth/refresh",
-                        "/api-docs/**",
-                        "/swagger-ui.html**"
-                ).permitAll()
-                .antMatchers("/api/**").authenticated()
-                .antMatchers("/users").denyAll()
+                .apply(new JwtConfigurer(tokenProvider))
                 .and()
-//                .cors()
-//                .and()
-                .apply(new JwtConfigurer(tokenProvider));
+                .build();
     }
 
 }
