5a353268f127907cb7eebe63cb24dbc1ed28ac7e
daruan-rst <daruan.rst@gmail.com>
2023-07-13 23:17:32 -0300

    1804 [DESAFIO] Criar os Testes de Integração do Controller de Login com XML

--- 18_ShieldingOurAPIWithTests/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/eurdio/integrationtests/controller/withxml/AuthControllerXmlTest.java
+++ 18_ShieldingOurAPIWithTests/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/eurdio/integrationtests/controller/withxml/AuthControllerXmlTest.java
@@ -1,10 +1,12 @@
-package br.com.eurdio.integrationtests.controller.withjson;
+package br.com.eurdio.integrationtests.controller.withxml;
 
 import br.com.eurdio.configs.TestConfigs;
 import br.com.eurdio.integrationtests.testcontainers.AbstractIntegrationTest;
 import br.com.eurdio.integrationtests.vo.AccountCredentialsVO;
 import br.com.eurdio.integrationtests.vo.TokenVO;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -16,19 +18,27 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class AuthControllerJsonTest extends AbstractIntegrationTest {
+public class AuthControllerXmlTest extends AbstractIntegrationTest {
 
     private static TokenVO tokenVO;
+
+    private static RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setContentType("application/xml")
+            .build();
 
     @Test
     @Order(1)
     public void testSignIn() throws JsonProcessingException {
         AccountCredentialsVO user = new AccountCredentialsVO("daruan", "admin123");
 
+
+
         tokenVO = given()
+                .spec(requestSpec)
+                .header("Accept", "application/xml")
                 .basePath("auth/signin")
                 .port(TestConfigs.SERVER_PORT)
-                .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .contentType(TestConfigs.CONTENT_TYPE_XML)
                 .body(user)
                 .when()
                 .post()
@@ -48,9 +58,11 @@ public class AuthControllerJsonTest extends AbstractIntegrationTest {
         AccountCredentialsVO user = new AccountCredentialsVO("daruan", "admin123");
 
         var newTokenVO = given()
+                .spec(requestSpec)
+                .header("Accept", "application/xml")
                 .basePath("auth/refresh")
                 .port(TestConfigs.SERVER_PORT)
-                .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .contentType(TestConfigs.CONTENT_TYPE_XML)
                     .pathParam("username", tokenVO.getUsername())
                     .header(TestConfigs.HEADER_PARAM_AUTHORIZATION, "Bearer " + tokenVO.getRefreshToken())
                 .when()
