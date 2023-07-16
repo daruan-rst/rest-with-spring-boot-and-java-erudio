128b55ecadef7c80a61f8bf5bb784c6b273be00c
daruan-rst <daruan.rst@gmail.com>
2023-07-16 14:52:00 -0300

    1807 [SOLUÇÃO] Implementando o YAML Mapper

--- 18_ShieldingOurAPIWithTests/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/eurdio/integrationtests/controller/withYaml/AuthControllerYamlTest.java
+++ 18_ShieldingOurAPIWithTests/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/eurdio/integrationtests/controller/withYaml/AuthControllerYamlTest.java
@@ -1,52 +1,77 @@
 package br.com.eurdio.integrationtests.controller.withYaml;
 
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertNotNull;
+
 import br.com.eurdio.configs.TestConfigs;
+import br.com.eurdio.integrationtests.controller.withYaml.mapper.YmlMapper;
 import br.com.eurdio.integrationtests.testcontainers.AbstractIntegrationTest;
 import br.com.eurdio.integrationtests.vo.AccountCredentialsVO;
 import br.com.eurdio.integrationtests.vo.TokenVO;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static io.restassured.RestAssured.given;
-import static io.restassured.config.EncoderConfig.encoderConfig;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.config.EncoderConfig;
+import io.restassured.config.RestAssuredConfig;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestMethodOrder(OrderAnnotation.class)
 public class AuthControllerYamlTest extends AbstractIntegrationTest {
 
+    private static YmlMapper objectMapper;
     private static TokenVO tokenVO;
 
-    private static ObjectMapper mapper =  new ObjectMapper(new YAMLFactory());
+    @BeforeAll
+    public static void setup() {
+        objectMapper = new YmlMapper();
+    }
 
     @Test
     @Order(1)
-    public void testSignIn() throws JsonProcessingException {
-        AccountCredentialsVO user = new AccountCredentialsVO("daruan", "admin123");
+    public void testSignin() throws JsonMappingException, JsonProcessingException {
 
-        String userYml = mapper.writeValueAsString(user);
-        tokenVO = given()
-                .config(RestAssured.config().encoderConfig(encoderConfig().encodeContentTypeAs("application/yml", ContentType.TEXT)))
-                .basePath("auth/signin")
+        AccountCredentialsVO user =
+                new AccountCredentialsVO("daruan", "admin123");
+
+        RequestSpecification specification = new RequestSpecBuilder()
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+                .build();
+
+        tokenVO = given().spec(specification)
+                .config(
+                        RestAssuredConfig
+                                .config()
+                                .encoderConfig(EncoderConfig.encoderConfig()
+                                        .encodeContentTypeAs(
+                                                TestConfigs.CONTENT_TYPE_YML,
+                                                ContentType.TEXT)))
+                .accept(TestConfigs.CONTENT_TYPE_YML)
+                .basePath("/auth/signin")
                 .port(TestConfigs.SERVER_PORT)
                 .contentType(TestConfigs.CONTENT_TYPE_YML)
-                .body(userYml)
+                .body(user, objectMapper)
                 .when()
                 .post()
                 .then()
                 .statusCode(200)
                 .extract()
                 .body()
-                .as(TokenVO.class);
+                .as(TokenVO.class, objectMapper);
 
         assertNotNull(tokenVO.getAccessToken());
         assertNotNull(tokenVO.getRefreshToken());
@@ -57,6 +82,14 @@ public class AuthControllerYamlTest extends AbstractIntegrationTest {
     public void testRefresh() throws JsonMappingException, JsonProcessingException {
 
         var newTokenVO = given()
+                .config(
+                        RestAssuredConfig
+                                .config()
+                                .encoderConfig(EncoderConfig.encoderConfig()
+                                        .encodeContentTypeAs(
+                                                TestConfigs.CONTENT_TYPE_YML,
+                                                ContentType.TEXT)))
+                .accept(TestConfigs.CONTENT_TYPE_YML)
                 .basePath("/auth/refresh")
                 .port(TestConfigs.SERVER_PORT)
                 .contentType(TestConfigs.CONTENT_TYPE_YML)
@@ -68,7 +101,7 @@ public class AuthControllerYamlTest extends AbstractIntegrationTest {
                 .statusCode(200)
                 .extract()
                 .body()
-                .as(TokenVO.class);
+                .as(TokenVO.class, objectMapper);
 
         assertNotNull(newTokenVO.getAccessToken());
         assertNotNull(newTokenVO.getRefreshToken());
