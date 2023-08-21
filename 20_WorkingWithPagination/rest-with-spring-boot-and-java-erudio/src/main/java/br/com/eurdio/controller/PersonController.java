4e0e61c95e288ba821bbbc3886fb7b37dd43c578
daruan-rst <daruan.rst@gmail.com>
2023-08-20 23:28:59 -0300

    2001 Apresentação da Seção - Query Params e Busca Paginada

---
+++ 20_WorkingWithPagination/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/eurdio/controller/PersonController.java
@@ -0,0 +1,137 @@
+package br.com.eurdio.controller;
+
+
+import br.com.eurdio.data.vO.v1.PersonVO;
+import br.com.eurdio.services.PersonServices;
+import br.com.eurdio.util.MediaType;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/person/v1")
+@Tag(name = "People", description = "Endpoints for managing People")
+public class PersonController {
+
+    @Autowired
+    private PersonServices service;
+
+    @GetMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+    @Operation(summary = "Finds all People", description = "Finds all People",
+            tags = {"People"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = PersonVO.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content),
+            })
+    public List<PersonVO> findAll() throws Exception {
+        return service.findAll();
+    }
+
+    @CrossOrigin(origins = "http://localhost:8080")
+    @GetMapping(value = "/{id}",
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+    @Operation(summary = "Finds a person", description = "Finds s person",
+            tags = {"People"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content (schema = @Schema(implementation = PersonVO.class))),
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content),
+            })
+    public PersonVO findById(@PathVariable(value = "id") Long id) throws Exception {
+        return service.findById(id);
+    }
+
+    @CrossOrigin(origins = {"http://localhost:8080", "https://erudio.com.br"})
+    @PostMapping(
+            consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+    @Operation(summary = "Adds a new person",
+            description = "Adds a new person in a JSON, XML or YML representation of a person",
+            tags = {"People"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content (schema = @Schema(implementation = PersonVO.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content),
+            })
+    public PersonVO create(@RequestBody PersonVO person) throws Exception {
+        return service.create(person);
+    }
+
+    @PutMapping(
+            consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+    @Operation(summary = "Updates a person",
+            description = "Updates a person in a JSON, XML or YML representation of a person",
+            tags = {"People"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content (schema = @Schema(implementation = PersonVO.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content),
+            })
+    public PersonVO update(@RequestBody PersonVO person) throws Exception {
+        return service.update(person);
+    }
+
+    @PatchMapping(value = "/{id}",
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+    @Operation(summary = "Disables a specific person by id", description = "Disables a specific person by id",
+            tags = {"People"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content (schema = @Schema(implementation = PersonVO.class))),
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content),
+            })
+    public PersonVO disablePerson(@PathVariable(value = "id") Long id) throws Exception {
+        return service.disablePerson(id);
+    }
+
+    @DeleteMapping(value = "/{id}",
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+    @Operation(summary = "Deletes a person",
+            description = "Deletes a person in a JSON, XML or YML representation of a person",
+            tags = {"People"},
+            responses = {
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content),
+            })
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) throws Exception {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
---
+++ 20_WorkingWithPagination/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/eurdio/services/AuthService.java
@@ -0,0 +1,62 @@
+package br.com.eurdio.services;
+
+import br.com.eurdio.data.vO.v1.security.AccountCredentialsVO;
+import br.com.eurdio.data.vO.v1.security.TokenVO;
+import br.com.eurdio.repository.UserRepository;
+import br.com.eurdio.security.jwt.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository repository;
+
+    public ResponseEntity signIn(AccountCredentialsVO data){
+        try{
+            var username = data.getUsername();
+            var password = data.getPassword();
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+
+            var user = repository.findByUsername(username);
+
+            var tokenResponse = new TokenVO();
+            
+            if (user != null){
+                tokenResponse = tokenProvider.createAccessToken(username, user.getRoles());
+            }else {
+                throw new UsernameNotFoundException("Username " + username + " not Found!");
+            }
+
+            return ResponseEntity.ok(tokenResponse);
+        }catch (Exception e){
+            throw new BadCredentialsException("Ivalid username/password supplied");
+        }
+    }
+
+    @SuppressWarnings("rawtypes")
+    public ResponseEntity refreshToken(String username, String refreshToken) {
+        var user = repository.findByUsername(username);
+
+        var tokenResponse = new TokenVO();
+        if (user != null) {
+            tokenResponse = tokenProvider.refreshToken(refreshToken);
+        } else {
+            throw new UsernameNotFoundException("Username " + username + " not found!");
+        }
+        return ResponseEntity.ok(tokenResponse);
+    }
+
+}
