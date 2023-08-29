75c442dfc9535a648c38e32eed67e25964905647
daruan-rst <daruan.rst@gmail.com>
2023-08-28 23:50:15 -0300

    2007 Passando Parâmetros ao Pageable

--- 20_WorkingWithPagination/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/eurdio/repository/PersonRepository.java
+++ 20_WorkingWithPagination/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/eurdio/repository/PersonRepository.java
@@ -1,6 +1,8 @@
 package br.com.eurdio.repository;
 
 import br.com.eurdio.model.Person;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -14,4 +16,7 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
     @Query("UPDATE Person p SET p.enabled = false where p.id =:id")
     void disablePerson(@Param("id") Long id);
 
+
+    @Query("SELECT p from Person where p.firstName LIKE LOWER(CONCAT ('%', :firstName, '%'))")
+    Page<Person> findPersonByName(@Param("firstName") String firstName, Pageable pageable);
 }
