80ac8fc4bfa7894a22ae79a1d451dc6b21e1fec3
daruan-rst <daruan.rst@gmail.com>
2023-09-18 22:24:36 -0300

    2105 Criando o Controller Responsável Pelo Upload e Download de Arquivos

---
+++ 21_WorkingWithFiles/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/eurdio/controller/FileController.java
@@ -0,0 +1,40 @@
+package br.com.eurdio.controller;
+
+
+import br.com.eurdio.data.vO.v1.UploadFileRespondeVO;
+import br.com.eurdio.services.FileStorageService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.logging.Logger;
+
+@Tag(name = "File Endpoint")
+@RestController
+@RequestMapping("/api/file/v1")
+public class FileController {
+
+        private Logger logger = Logger.getLogger(FileController.class.getName());
+
+        @Autowired
+        private FileStorageService service;
+
+        @PostMapping("/upload-file")
+        public UploadFileRespondeVO uploadFile(@RequestParam("file") MultipartFile file){
+            logger.info("Storing file to disk");
+            String fileName = service.storeFile(file);
+            String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                    .path("/api/file/v1/download-file/")
+                    .path(fileName)
+                    .toUriString();
+
+            return new UploadFileRespondeVO(
+                    fileName, fileDownloadUri, file.getContentType(), file.getSize());
+        }
+
+}
